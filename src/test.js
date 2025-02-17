5b3adda4be122a513a62a52384c22170097b9db4
Asep Supriyadi <asepsupyad789@gmail.com>
2025-02-17 15:11:40 +0700

    Get ridd of increment in the test script

--- src/test.js
+++ src/test.js
@@ -2,8 +2,6 @@ import { getAccountDetails } from "./helper/account.js";
 import {getUserPowerBIDashboardByID, getUserPowerBIDashboards} from "./helper/category-dashboard.js";
 import {getPresignedS3URL, getUserCategoryInsights} from "./helper/category-report.js";
 
-let totalRequests = 0;
-
 export default function () {
     // Shows ENV to make sure that K6 can read stored ENV
     console.info(__ENV.TEST_ENV);
@@ -13,22 +11,12 @@ export default function () {
 
     // USER SCENARIOS
     getAccountDetails();
-    totalRequests++;
 
     // CATEGORY DASHBOARD SCENARIOS
     getUserPowerBIDashboards();
-    totalRequests++;
     getUserPowerBIDashboardByID();
-    totalRequests++;
 
     // CATEGORY REPORT SCENARIOS
     getUserCategoryInsights();
-    totalRequests++;
     getPresignedS3URL();
-    totalRequests++;
 }
-
-export function teardown() {
-    console.log(`Total API requests executed: ${totalRequests}`);
-}
-
